a87bd092374aa79200e46783ef83a534159e55ec
kirigamine <32932020+kirigamine@users.noreply.github.com>
2020-02-01 12:58:42 +0900

    add itigo
    人物増やす、矢印色変更

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var nodes = null;
+﻿var nodes = null;
 var edges = null;
 var network = null;
 var IMG = "img/";
@@ -22,41 +22,62 @@ var nodes = [
   { id: 15, shape: "image", image: IMG + "1f48e.png"}, // Siren
   { id: 16, shape: "image", image: IMG + "1f436.png"}, // Sabaka
   { id: 17, shape: "image", image: IMG + "1f426.png"}, // Passero
+  { id: 18, shape: "image", image: IMG + "1f36d.png"}, // Koo
+  { id: 19, shape: "image", image: IMG + "1f36c.png"}, // Fee
+  { id: 20, shape: "image", image: IMG + "1f353.png"}, // Raj
 ];
 // create an array with edges
 var edges = [
-  { from: 1, to: 2, label: "夫婦",  arrows: "to, from"},
-  { from: 1, to: 6, label: "部下、妾"},
-  { from: 1, to: 7, label: "部下、元妾"},
+  { from: 1, to: 2, label: "夫婦",  arrows: "to, from", color: "magenta"},
+  { from: 1, to: 6, label: "部下"},
+  { from: 1, to: 7, label: "部下"},
   { from: 1, to: 8, label: "部下"},
   { from: 1, to: 9, label: "部下"},
   { from: 2, to: 3, label: "部下"},
   { from: 2, to: 4, label: "部下"},
   { from: 2, to: 5, label: "部下"},
+  { from: 7, to: 20, label: "部下"},
+  { from: 18, to: 6, label: "因縁"},
+  { from: 19, to: 6, label: "因縁"},
+  { from: 11, to: 18, label: "保護"},
+  { from: 11, to: 19, label: "保護"},
   { from: 1, to: 14, label: "師匠"},
   { from: 5, to: 14, label: "師匠"},
   { from: 14, to: 1, label: "弟子"},
   { from: 14, to: 5, label: "弟子"},
-  { from: 2, to: 6, label: "妾"},
   { from: 2, to: 7, label: "元妾"},
   { from: 6, to: 10, label: "元夫婦", arrows: "to, from"},
-  { from: 3, to: 4, label: "恋人",  arrows: "to, from"},
-  { from: 8, to: 9, label: "恋人",  arrows: "to, from"},
-  { from: 7, to: 10, label: "恋人",  arrows: "to, from"},
-  { from: 11, to: 12, label: "恋人",  arrows: "to, from"},
-  { from: 13, to: 14, label: "恋人",  arrows: "to, from"},
-  { from: 15, to: 16, label: "恋人",  arrows: "to, from"},
+  { from: 3, to: 4, label: "恋人、[三童子]",  arrows: "to, from", color: "magenta"},
+  { from: 8, to: 9, label: "恋人、[四翼騎士]",  arrows: "to, from", color: "magenta"},
+  { from: 7, to: 10, label: "恋人",  arrows: "to, from", color: "magenta"},
+  { from: 11, to: 12, label: "恋人",  arrows: "to, from", color: "magenta"},
+  { from: 13, to: 14, label: "恋人",  arrows: "to, from", color: "magenta"},
+  { from: 15, to: 16, label: "恋人",  arrows: "to, from", color: "magenta"},
+  { from: 3, to: 5, label: "[三童子]",  arrows: "to, from", color: "red"},
+  { from: 4, to: 5, label: "[三童子]",  arrows: "to, from", color: "red"},
+  { from: 6, to: 7, label: "[四翼騎士]",  arrows: "to, from", color: "blue"},
+  { from: 6, to: 8, label: "兄弟弟子、[四翼騎士]",  arrows: "to, from", color: "blue"},
+  { from: 6, to: 9, label: "[四翼騎士]",  arrows: "to, from", color: "blue"},
+  { from: 7, to: 8, label: "[四翼騎士]",  arrows: "to, from", color: "blue"},
+  { from: 7, to: 9, label: "[四翼騎士]",  arrows: "to, from", color: "blue"},
   { from: 9, to: 17, label: "友人",  arrows: "to, from"},
-  { from: 7, to: 12, label: "兄妹",  arrows: "to, from"},
-  { from: 2, to: 10, label: "兄弟",  arrows: "to, from"},
-  { from: 13, to: 15, label: "兄弟",  arrows: "to, from"},
-  { from: 6, to: 8, label: "兄弟弟子",  arrows: "to, from"},
-  { from: 1, to: 13, label: "親子",  arrows: "to, from"},
-  { from: 2, to: 13, label: "親子",  arrows: "to, from"},
-  { from: 1, to: 15, label: "親子",  arrows: "to, from"},
-  { from: 2, to: 15, label: "親子",  arrows: "to, from"},
-  { from: 6, to: 16, label: "親子",  arrows: "to, from"},
-  { from: 10, to: 16, label: "親子",  arrows: "to, from"}
+  { from: 7, to: 12, label: "兄妹",  arrows: "to, from", color: "green"},
+  { from: 2, to: 10, label: "兄弟",  arrows: "to, from", color: "green"},
+  { from: 13, to: 15, label: "兄弟",  arrows: "to, from", color: "green"},
+  { from: 18, to: 19, label: "双子",  arrows: "to, from", color: "green"},
+  { from: 1, to: 13, label: "親",   color: "green"},
+  { from: 1, to: 13, label: "子",   arrows: "from", color: "lightgreen"},
+  { from: 2, to: 13, label: "親",   color: "green"},
+  { from: 2, to: 13, label: "子",   arrows: "from", color: "lightgreen"},
+  { from: 1, to: 15, label: "親",   color: "green"},
+  { from: 1, to: 15, label: "子",   arrows: "from", color: "lightgreen"},
+  { from: 2, to: 15, label: "親",   color: "green"},
+  { from: 2, to: 15, label: "子",   arrows: "from", color: "lightgreen"},
+  { from: 6, to: 16, label: "親",   color: "green"},
+  { from: 6, to: 16, label: "子",   arrows: "from", color: "lightgreen"},
+  { from: 10, to: 16, label: "親",  color: "green"},
+  { from: 10, to: 16, label: "子",  arrows: "from", color: "lightgreen"}
+
 
 ];
 
@@ -66,7 +87,7 @@ var data = {
   nodes: nodes,
   edges: edges
 };
-var options = { physics: true,  edges: {arrows: "to", font: { align: "horizontal"} }};
+var options = { physics: true,  edges: {arrows: "to", color: "#999" , font: { align: "horizontal"} }};
 var network = new vis.Network(container, data, options);
 
 window.addEventListener("load", () => {
